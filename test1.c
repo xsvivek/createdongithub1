ae853ba85f3624e94cb9bb0b0acf712a33a16e87
xsvivek <xsvk@gmx.com>
2019-04-20 18:27:55 +0530

    New (second) printf line added in test.c file

--- test1.c
+++ test1.c
@@ -2,4 +2,6 @@
 void main()
 {
     printf("So...This is just for the testing purposes related to GITHUB");
+
+    printf("Now, This is the new change i am making using git command from vscode")
 } 
